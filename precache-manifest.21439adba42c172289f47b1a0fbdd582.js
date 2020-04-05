51221bd5f028914f9a1f7868d1cd17f20f502cd3
Yanlian Xin <xinyanlian@outlook.com>
2020-04-05 16:29:59 -0400

    Updates

---
+++ precache-manifest.21439adba42c172289f47b1a0fbdd582.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab11baaecfba9f32b1af1423d1ea847",
+    "url": "/redux-songs/index.html"
+  },
+  {
+    "revision": "910397742bb827185126",
+    "url": "/redux-songs/static/js/2.bc41d29f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-songs/static/js/2.bc41d29f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b9698e729b1e58774f9",
+    "url": "/redux-songs/static/js/main.2ec9f6e5.chunk.js"
+  },
+  {
+    "revision": "5a4d7d4c5c5c49ee2fba",
+    "url": "/redux-songs/static/js/runtime-main.a8908585.js"
+  }
+]);
